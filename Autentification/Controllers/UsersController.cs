b6b5da24a2b904532004dc20aab1c32a5bce5c4e
sucurei <62082711+sucurei@users.noreply.github.com>
2020-12-03 16:33:13 +0200

    Add files via upload

---
+++ Autentification/Controllers/UsersController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Autentification.Data;
+using Autentification.Model;
+
+namespace Autentification.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserRepository _repository;
+
+        public UsersController(IUserRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<User>> Get()
+        {
+            return await _repository.GetAll();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetById(string id)
+        {
+            User user = await _repository.GetById(id);
+            if (user == null) return NotFound();
+            return user;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Insert(User user)
+        {
+            await _repository.AddUser(user);
+            return CreatedAtAction("GetById", new { id = user.Id }, user);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<User>> Update(string id, User user)
+        {
+            if (id != user.Id) return BadRequest();
+
+            User requestedUser = await _repository.GetById(id);
+            if (requestedUser == null) return NotFound();
+            return user;
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            User requestedUser = await _repository.GetById(id);
+            if (requestedUser == null) return NotFound();
+            await _repository.DeleteUser(id);
+            return NoContent();
+        }
+    }
+}
---
+++ Autentification/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Autentification.Controllers;
+using Autentification.Model;
+using MongoDB.Driver;
+
+namespace Autentification.Data
+{
+    public class DataContext
+    {
+        public DataContext(IUsersDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+            Users = database.GetCollection<User>(settings.UserCollectionName);
+        }
+
+        public IMongoCollection<User> Users { get; }
+    }
+}
