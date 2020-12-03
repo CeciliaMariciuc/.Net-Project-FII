e9d930a6e403e8d653baebeae9750fba7ac18566
teodoratrinca <61377986+teodoratrinca@users.noreply.github.com>
2020-12-03 16:05:07 +0200

    Add files via upload

---
+++ PlayNet/Rating.Microservices/Model/RatingDatabaseSettings.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Rating.Microservice.Model
+{
+    public class RatingsDatabaseSettings : IRatingsDatabaseSettings
+    {
+        public string RatingsCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+
+    public interface IRatingsDatabaseSettings
+    {
+        string RatingsCollectionName { get; set; }
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+    }
+}
