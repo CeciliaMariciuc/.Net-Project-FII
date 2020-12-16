3c0c3d58dbcc0401dd482ac24c0fd2480dfcc6dc
CeciliaMariciuc <ceci_mariciuc@yahoo.com>
2020-12-16 21:49:31 +0200

    unit tests for authetication.microservice

---
+++ PlayNet/Authentication-Tests/CryptServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Authentication.Microservice.Helpers;
+
+using Xunit;
+
+namespace Authentication_Tests
+{
+    public class CryptServiceTest
+    {
+
+        [Fact]
+        public void Decrypt_Encryption_Equals_PlainText()
+        {
+            CryptService cryptService = new CryptService();
+            string plainText = "text_to_encrypt";
+
+            string encryption = cryptService.EncryptString(plainText);
+            string decryption = cryptService.DecryptString(encryption);
+
+            Assert.Equal(plainText, decryption);
+        }
+    }
+}
---
+++ PlayNet/Authentication-Tests/UserRepositoryTests.cs
@@ -0,0 +1,50 @@
+﻿using Autentification.Data;
+using Autentification.Model;
+using MongoDB.Driver;
+using Moq;
+using System;
+using Xunit;
+
+namespace Authentication_Tests
+{
+    public class UserRepositoryTests
+    {
+        private readonly Mock<IUsersDatabaseSettings> _mockContext;
+        public UserRepositoryTests()
+        {
+            _mockContext = new Mock<IUsersDatabaseSettings>();
+        }
+
+        [Fact]
+        public async void UserRepository_Create_NullUser_Failure()
+        {
+            // Arrange
+            User _user = null;
+
+            //Act 
+            _mockContext.Setup((p => p.UserCollectionName)).Returns("users");
+            _mockContext.Setup((p => p.ConnectionString)).Returns("mongodb://localhost:27017");
+            _mockContext.Setup((p => p.DatabaseName)).Returns("AuthenticationDB");
+            var userRepository = new UserRepository(_mockContext.Object);
+
+            // Assert
+            await Assert.ThrowsAsync<ArgumentNullException>(() => userRepository.AddUser(_user));
+        }
+
+        [Fact]
+        public async void UserRepository_NoUser_With_One_Letter_Email()
+        {
+            // Arrange
+            string email = "a";
+
+            //Act 
+            _mockContext.Setup((p => p.UserCollectionName)).Returns("users");
+            _mockContext.Setup((p => p.ConnectionString)).Returns("mongodb://localhost:27017");
+            _mockContext.Setup((p => p.DatabaseName)).Returns("AuthenticationDB");
+            var userRepository = new UserRepository(_mockContext.Object);
+
+            // Assert
+            Assert.False(await userRepository.IsEmailUsed(email));
+        }
+    }
+}
