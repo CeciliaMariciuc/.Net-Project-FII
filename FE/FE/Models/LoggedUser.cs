bda99c3af42f7233b8bb078f761722d799726fa8
teodoratrinca <teodora.trinca@gmail.com>
2020-12-17 20:14:26 +0200

    FrontEnd updates

---
+++ FE/FE/Models/LoggedUser.cs
@@ -0,0 +1,12 @@
+﻿
+namespace FE.Models
+{
+    public class LoggedUser
+    {
+        public string Token { get; set; }
+
+        public string UserId { get; set; }
+
+        public string Email { get; set; }
+    }
+}
